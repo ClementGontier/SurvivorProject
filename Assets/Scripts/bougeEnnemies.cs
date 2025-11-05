69b425cf5c6d5be4a035906d634d62a88b9c76f2
M3rwin <marcoetude@gmail.com>
2025-11-05 07:55:56 +0100

    Update bougeEnnemies.cs

--- Assets/Scripts/bougeEnnemies.cs
+++ Assets/Scripts/bougeEnnemies.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class bougeEnnemies : MonoBehaviour
@@ -20,7 +21,39 @@ public class bougeEnnemies : MonoBehaviour
         Vector2 direction = (joueur.transform.position - transform.position).normalized;
 
         // déplacement
-        transform.position += (Vector3)(direction * speed * Time.deltaTime);
-        animator.Play("Move");
+        Vector3 move = (Vector3)(direction * speed * Time.deltaTime);
+
+        float moveX = move.x, moveY = move.y;
+
+
+        if (moveX != 0)
+        {
+            if (moveX > 0)
+            {
+                joueur.GetComponent<SpriteRenderer>().flipX = false;
+                animator.SetBool("MoveTrigger", true);
+            }
+            else if (moveX < 0)
+            {
+                joueur.GetComponent<SpriteRenderer>().flipX = true;
+                animator.SetBool("MoveTrigger", true);
+            }
+        }
+        else if (moveY != 0)
+        {
+            if (moveY > 0)
+            {
+                animator.SetBool("MoveTrigger", true);
+            }
+            else if (moveY < 0)
+            {
+                animator.SetBool("MoveTrigger", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("MoveTrigger", false);
+        }
+        transform.position += move;
     }
 }
