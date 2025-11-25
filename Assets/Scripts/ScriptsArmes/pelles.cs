4826bcbde47d64e1f2276635394d8904f7d7023e
M3rwin <marcoetude@gmail.com>
2025-11-25 15:15:47 +0100

    ajout des pelles
    le perso peut aussi tirer des pelles

---
+++ Assets/Scripts/ScriptsArmes/pelles.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class pelles : MonoBehaviour, IWeapon
+{
+    public float vitesseAttaque = 1f;
+    protected float tempsAvantProchaineAttaque = 0f;
+    public float vitesseProjectile = 10f;
+    public int degats = 10;
+    protected GameObject projectilePrefab;
+    public GameObject departTire;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        projectilePrefab = Resources.Load<GameObject>("Prefab/Armes/projPelles");
+    }
+
+    // Update is called once per frame
+    public void updateWeapon()
+    {
+        // tant que le temps avant la prochaine attaque est supérieur à 0 on le décrémente
+        if (tempsAvantProchaineAttaque > 0)
+        {
+            tempsAvantProchaineAttaque -= Time.deltaTime;
+        }
+    }
+    
+    public void essaieAttaque(GameObject ennemie)
+    {
+        if(tempsAvantProchaineAttaque<=0)
+        {
+            attaque(ennemie);
+            tempsAvantProchaineAttaque = 1f / vitesseAttaque;
+        }
+    }
+
+    public void attaque(GameObject ennemie)
+    {
+        // on calcule la direction du projectile
+        Vector3 emplacementEnnemie = ennemie.GetComponent<Collider2D>().bounds.center;
+        Vector3 direction = (emplacementEnnemie - departTire.transform.position).normalized;
+
+        // on calcule l'orientation du projectile
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // on crée le projectile
+        GameObject projectile = Instantiate(projectilePrefab, departTire.transform.position, Quaternion.identity);
+
+        // on oriente le projectile (j'ai mis +270 à l'angle pour que le sprite soit dans le bon sens, sinon il était perpendiculaire à la direction et j'ai la flemme de savoir pourquoi)
+        projectile.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle+270));
+
+        // on assigne les dégâts au projectile
+        projectilesPellesManager degatProj = projectile.GetComponent<projectilesPellesManager>();
+        if (degatProj != null)
+        {
+            degatProj.degats = degats;
+        }
+
+        // on annule la gravité et on applique une vélocité au projectile
+        Rigidbody2D rbP = projectile.GetComponent<Rigidbody2D>();
+        if (rbP != null)
+        {
+            rbP.gravityScale = 0;
+            rbP.linearVelocity = direction.normalized * vitesseProjectile;
+        }
+    }
+
+}
