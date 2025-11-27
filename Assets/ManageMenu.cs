5d9d843097b63cce9abc14bcba861a3879b9f10a
ClementGontier <gontierclement3@gmail.com>
2025-11-27 11:24:56 +0100

    refonte

---
+++ Assets/ManageMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void Niveau1()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
