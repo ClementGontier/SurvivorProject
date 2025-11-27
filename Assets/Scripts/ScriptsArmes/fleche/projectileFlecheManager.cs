dbd14b821fd52dcf38e7f66334ba7827b0704033
M3rwin <marcoetude@gmail.com>
2025-11-27 11:24:56 +0100

    debut ajout arme fleches

--- Assets/Scripts/ScriptsArmes/fleche/projectileFlecheManager.cs
+++ Assets/Scripts/ScriptsArmes/fleche/projectileFlecheManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class auraManager : MonoBehaviour
+public class projectileFlecheManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
