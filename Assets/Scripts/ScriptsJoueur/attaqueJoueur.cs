f6a86b7cf964acb28d67171362c3109908f70ffe
M3rwin <marcoetude@gmail.com>
2025-11-27 12:14:39 +0100

    Revert "Merge pull request #5 from ClementGontier/marco"
    This reverts commit f2dbf2b7e3da695190f0d368735d24d5ae5682e5, reversing
    changes made to a784c7f700b8203267d8fd8451842eac8ed2c390.

---
+++ Assets/Scripts/ScriptsJoueur/attaqueJoueur.cs
@@ -0,0 +1,60 @@
+using System.Numerics;
+using UnityEngine;
+
+public class attaqueJoueur : MonoBehaviour
+{
+    public float vitesseAttaque = 1f;
+    protected float tempsAvantProchaineAttaque = 0f;
+    public float vitesseProjectile = 10f;
+    public int degats = 10;
+    protected GameObject projectilePrefab;
+    public GameObject departTire;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        projectilePrefab = Resources.Load<GameObject>("Prefab/Armes/projectile");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // tant que le temps avant la prochaine attaque est supérieur à 0 on le décrémente
+        if (tempsAvantProchaineAttaque > 0)
+        {
+            tempsAvantProchaineAttaque -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "ennemie")
+        {
+            essaieAttaque(trigger.gameObject);
+        }
+    }
+    
+    private void essaieAttaque(GameObject ennemie)
+    {
+        if(tempsAvantProchaineAttaque<=0)
+        {
+            attaque(ennemie);
+            tempsAvantProchaineAttaque = 1f / vitesseAttaque;
+        }
+    }
+
+    private void attaque(GameObject ennemie)
+    {
+        UnityEngine.Vector3 emplacementEnnemie = ennemie.GetComponent<Collider2D>().bounds.center;
+        UnityEngine.Vector3 direction = (emplacementEnnemie - departTire.transform.position).normalized;
+
+        GameObject projectile = Instantiate(projectilePrefab, departTire.transform.position, UnityEngine.Quaternion.identity);
+        Rigidbody2D rbP = projectile.GetComponent<Rigidbody2D>();
+        if (rbP != null)
+        {
+            rbP.gravityScale = 0;
+            rbP.linearVelocity = direction.normalized * vitesseProjectile;
+        }
+    }
+}
