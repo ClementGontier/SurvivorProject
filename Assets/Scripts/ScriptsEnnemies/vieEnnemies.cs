60cb7fb3feb34103cdcb007e6d8cca20bac84843
M3rwin <marcoetude@gmail.com>
2025-12-03 10:44:34 +0100

    ajout xp

--- Assets/Scripts/ScriptsEnnemies/vieEnnemies.cs
+++ Assets/Scripts/ScriptsEnnemies/vieEnnemies.cs
@@ -5,6 +5,7 @@ public class vieEnnemies : MonoBehaviour
     public int vieMax;
     protected int vieActuelle;
     public int degats;
+    public GameObject xpPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +24,16 @@ public class vieEnnemies : MonoBehaviour
     
     void meurt()
     {
+        dropExp();
         Destroy(gameObject);
+    }
+
+    void dropExp()
+    {
+        int nb = Random.Range(1, 4); 
+        if (nb == 3)
+        {
+            Instantiate(xpPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
